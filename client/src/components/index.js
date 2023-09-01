e79936935ec6c07e1e32ed22bff17fc84035621d
orliyahTheCoder <orliyahisrael@gmail.com>
2023-09-01 09:39:40 +0200

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPicker";
+import AIPicker from "./AIPicker";
+import FilePicker from "./FilePicker";
+import Tabs from "./Tabs";
+
+export { CustomButton, ColorPicker, AIPicker, FilePicker, Tabs };
